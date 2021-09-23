944bd7769544ffcf7ca7f423314313e461056099
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-09-23 16:53:26 +0200

    footer css

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,2 +1,20 @@
+<div class="top-footer">
+    <div class="container">
+        <div class="row top-footer-inner">
+            <div class="col-2">Conttaci</div>
+            <div class="col-2">Aiuto</div>
+            <div class="col-2">Chi siamo</div>
+            <div class="col-2">Carica Articolo</div>
+            <div class="col-2">Newsletters</div>
+            <div class="col-2">Servizio Clienti</div>
+        </div>
+    </div>
+</div>
 
-footer
+<div class="bottom-footer">
+    <div class="container">
+        <div class="bottom-footer-inner">
+            <div class="company">The News Network S.p.A. - P.Iva 015000051009 - ISSN 2459-0847</div>
+        </div>
+    </div>
+</div>
