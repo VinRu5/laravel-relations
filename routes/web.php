c8b1dd7bb594c7a15ec35a0caf95a956936ada2b
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-09-23 16:28:19 +0200

    header

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::resource('articles', 'BackOfficeController');
